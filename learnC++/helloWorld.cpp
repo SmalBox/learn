68bb6ee16b2f8e415a4bf42282d6e4b51c847fcc
tps1Mac <smal_box@163.com>
2018-08-06 21:23:27 +0800

    add vscode config file for mac, test file helloWorld, gitignore file

---
+++ learnC++/helloWorld.cpp
@@ -0,0 +1,16 @@
+/*************************************************************************
+    > File Name: helloWorld.cpp
+    > Author: zcy
+    > Email: zcy505414969@gmail.com
+    > Created Time: 一  8/ 6 00:00:26 2018
+ ************************************************************************/
+#include<iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    cout<<"hello world!"<<endl;
+    cout<<"你好，世界！"<<endl;
+    return 0;
+}
+
+
